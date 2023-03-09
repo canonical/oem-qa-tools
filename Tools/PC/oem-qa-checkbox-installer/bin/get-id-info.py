a9e04b455f970e122883e36d24fcce50833921ec
liaou3 <vincent.liao@canonical.com>
2023-03-09 16:17:30 +0800

    oem-qa-checkbox-installer first commit

---
+++ Tools/PC/oem-qa-checkbox-installer/bin/get-id-info.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import configparser
+
+print("\nGetting ID info from C3...")
+
+config = configparser.ConfigParser()
+config.read("./conf/setting.conf")
+c3cfg = config["c3"]
+username = c3cfg["username"]
+api_key = c3cfg["api_key"]
+cid = input("Please enter CID: ")
+
+url = f"https://certification.canonical.com/api/v1/hardware/{cid}/?username={username}&api_key={api_key}&format=json"
+data = requests.get(url)
+data_req = json.loads(data.text)
+
+with open("hardware_info.txt", "wt") as f:
+    f.write(f"CID:\t\t{cid}\n")
+    f.write(f"Secure ID:\t{data_req.get('secure_id')}\n")
+    f.write(f"C3:\t\thttps://certification.canonical.com/hardware/{cid}\n")
+    f.write(f"SKU:\t\t{data_req.get('sku')}\n")
---
+++ Tools/PC/oem-qa-checkbox-installer/bin/install-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\nInstalling other essential packages..."
+
+pwd='u'
+
+# Install bugit
+echo "Installing bugit..."
+echo ${pwd} | sudo -S snap install bugit --edge --devmode
+
+# Install sosreport
+echo "Installing sosreport..."
+echo ${pwd} | sudo -S snap install sosreport --classic
+
+# Install openssh
+echo "Installing openssh-server..."
+echo ${pwd} | sudo apt install -y openssh-server
+echo "Enabling ssh.service..."
+echo ${pwd} | sudo systemctl enable ssh --now
