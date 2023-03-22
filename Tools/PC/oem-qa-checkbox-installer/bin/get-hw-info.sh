0d3e2c12bebcd0617ffe349d2954c8696b82775f
liaou3 <vincent.liao@canonical.com>
2023-03-22 14:02:02 +0800

    Modify a more accurate way to get the hardware information and also prevent from permission error

--- Tools/PC/oem-qa-checkbox-installer/bin/get-hw-info.sh
+++ Tools/PC/oem-qa-checkbox-installer/bin/get-hw-info.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+pwd='u'
+
 echo -e "\nGetting hardware info..."
 
-myCPU=$(sudo dmidecode -s processor-version)
-myGPU=$(lspci -nnv | grep "VGA controller\|NVIDIA\|Display controller" | cut -c 9- | sed '2,4s/^/		/')
+# Get hardware info
+myCPU=$(echo ${pwd} | sudo -S dmidecode -s processor-version)
+myGPU=$(lspci -nnv | awk -F ': ' '/VGA controller|NVIDIA|Display controller/{for(i=2;i<=NF;i++)printf("%s ",$i);print ""}')
 myWireless=$(lspci -nnv | grep 'Network controller' | cut -c 36-)
 myWiFiSub=$(lspci -nnv | grep -A 2 'Network controller' | grep -i subsystem | cut -c 13-)
 myManifest=$(ubuntu-report show | grep DCD | awk '{print $2}' | sed 's/"//g')
-myKernel=$(uname -a)
-myBIOSvers=$(sudo dmidecode -s bios-version)
+myKernel=$(uname -r)
+myBIOSvers=$(echo ${pwd} | sudo -S dmidecode -s bios-version)
 
 {
     echo -e "CPU:\t\t${myCPU} "
