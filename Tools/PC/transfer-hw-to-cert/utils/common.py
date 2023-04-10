3e5266b218f2dfbed4fae4e5a0af8b5a864c6a51
baconyao <baconyao@baconyaode-MacBook-Air.local>
2023-04-10 10:45:52 +0800

    Add configs and common function

--- Tools/PC/transfer-hw-to-cert/utils/common.py
+++ Tools/PC/transfer-hw-to-cert/utils/common.py
@@ -1,3 +1,4 @@
+import json
 import re
 
 
@@ -14,3 +15,35 @@ def is_valid_location(location: str) -> bool:
     pattern = re.compile(
         r'^TEL-L\d-F\d{2}-S\d-P[12]$|^TEL-L\d-R\d{2}-S\d{1,2}-P0$')
     return True if re.match(pattern, location) else False
+
+
+def read_json_config(config_path: str) -> dict:
+    if not config_path.endswith(".json"):
+        raise Exception(f"Expect JSON config file but got {config_path}")
+    with open(config_path) as config_file:
+        file_contents = config_file.read()
+        return json.loads(file_contents)
+
+
+def parse_location(location: str) -> dict:
+    """ Parse the location data
+    """
+    return_dict = {
+        'Lab': '',
+        'Frame': '',
+        'Shelf': '',
+        'Partition': ''
+    }
+    part_re = re.compile(
+        '(?P<Lab>TEL-L\d)-(?P<Frame>F\d+)-S(?P<Shelf>\d+)-P(?P<Partition>\d+)'  # noqa: W605, E501
+    )
+    match = re.search(part_re, location)
+    if match:
+        return_dict = {
+            'Lab': match.group('Lab'),
+            'Frame': match.group('Frame'),
+            'Shelf': match.group('Shelf'),
+            'Partition': match.group('Partition')
+        }
+
+    return return_dict
