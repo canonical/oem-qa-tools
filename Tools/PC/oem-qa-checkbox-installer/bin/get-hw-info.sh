59036ec2b42424f99f30c58e819b5c7929659a25
LiaoU3 <58060146+LiaoU3@users.noreply.github.com>
2023-04-19 16:57:13 +0800

    Modify get-hw-info and get-id-info (#7)
    * Modify a more accurate way to get the hardware information and also prevent from permission error
    
    * Replace echo with printf to get a more accurate output and also replace '\t' with ' ' to fix the tabbing problem

--- Tools/PC/oem-qa-checkbox-installer/bin/get-hw-info.sh
+++ Tools/PC/oem-qa-checkbox-installer/bin/get-hw-info.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-echo -e "\nGetting hardware info..."
+pwd='u'
+output_file='./hardware_info.txt'
 
-myCPU=$(sudo dmidecode -s processor-version)
-myGPU=$(lspci -nnv | grep "VGA controller\|NVIDIA\|Display controller" | cut -c 9- | sed '2,4s/^/		/')
+printf "\nGetting hardware info...\n"
+
+# Get hardware info
+myCPU=$(echo ${pwd} | sudo -S dmidecode -s processor-version)
+myGPU=$(lspci -nnv | awk -F ': ' '/VGA controller|NVIDIA|Display controller/{for(i=2;i<=NF;i++)printf("%s ",$i);print ""}')
 myWireless=$(lspci -nnv | grep 'Network controller' | cut -c 36-)
 myWiFiSub=$(lspci -nnv | grep -A 2 'Network controller' | grep -i subsystem | cut -c 13-)
 myManifest=$(ubuntu-report show | grep DCD | awk '{print $2}' | sed 's/"//g')
-myKernel=$(uname -a)
-myBIOSvers=$(sudo dmidecode -s bios-version)
+myKernel=$(uname -r)
+myBIOSvers=$(echo ${pwd} | sudo -S dmidecode -s bios-version)
 
+# output hardware info to output file
 {
-    echo -e "CPU:\t\t${myCPU} "
-    echo -e "GPU:\t\t${myGPU} "
-    echo -e "Wireless:\t\t${myWireless} "
-    echo -e "WiFi Sub ID:\t${myWiFiSub} "
-    echo -e "Manifest Info:\t${myManifest}"
-    echo -e "Kernel Version:\t${myKernel}"
-    echo -e "BIOS:\t\t${myBIOSvers} "
-}   >> "./hardware_info.txt"
+    printf "CPU: %s\n" "${myCPU}"
+    printf "GPU: %s\n" "${myGPU}"
+    printf "Wireless: %s\n" "${myWireless}"
+    printf "WiFi Sub ID: %s\n" "${myWiFiSub}"
+    printf "Manifest Info: %s\n" "${myManifest}"
+    printf "Kernel Version: %s\n" "${myKernel}"
+    printf "BIOS: %s\n" "${myBIOSvers}"
+} >> "${output_file}"
