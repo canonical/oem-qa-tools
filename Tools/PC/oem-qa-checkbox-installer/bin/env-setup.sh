30011b1ba66b44404963ace45994bfdc3cd00298
hanhsuan <hanhsuan.lee@canonical.com>
2023-07-08 13:47:08 +0800

    Add to set current to autologin while checkbox installation.
    This commit also change to use dconf load to set testing environment to
    make it easier to restore

--- Tools/PC/oem-qa-checkbox-installer/bin/env-setup.sh
+++ Tools/PC/oem-qa-checkbox-installer/bin/env-setup.sh
@@ -15,17 +15,13 @@ APT::Periodic::Unattended-Upgrade "0";
 EOF
 echo -e "\033[1;42;37mdone\033[0m"
 
-# Disable auto suspend when plugged in
-echo -e "\033[1;32mDisable auto suspend when plugged in\033[0m"
-# Settings > Power > Power > Automatic Suspend > Plugged in > Off
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type nothing && echo -e "\033[1;42;37mdone\033[0m"
+# back up dconf setting
+echo -e "\033[1;32mBack up gnome setting by dconf dump\033[0m"
+dconf dump / > ./conf/env-for-restore-dconf
 
-# Disable auto suspend when on battery power
-echo -e "\033[1;32mDisable auto suspend when on battery\033[0m"
-# Settings > Power > Power > Automatic Suspend > On Battery Power > Off
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type nothing && echo -e "\033[1;42;37mdone\033[0m"
+# change dconf setting for checkbox testing
+echo -e "\033[1;32mChanging setting by dconf load\033[0m"
+dconf load / < ./conf/checkbox-testing-env-dconf
 
-# Disable auto blank screen
-echo -e "\033[1;32mDisable auto blank screen\033[0m"
-# Settings > Privacy > Screen > Blank Screen Delay > Never
-gsettings set org.gnome.desktop.session idle-delay 0 && echo -e "\033[1;42;37mdone\033[0m"
+# set current to be autologing
+sudo ./bin/autologin.sh $(whoami)
