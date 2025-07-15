c043b690ca64f3b99607b798e64a49f7477515c5
Zhongning Li <zhongning.li@canonical.com>
2025-07-15 17:09:19 +0800

    feat: 1st provision data impl

---
+++ src/testflinger-yaml-sdk/models/base.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+from typing import Literal, override
+
+from models.provision_data import ProvisionData
+from models.test_data import TestData
+
+
+@dataclass
+class SshKeyProvider:
+    # lp: launchpad
+    # gh: github
+    provider_name: Literal["lp", "gh"]
+    username: str  # username on launchpad/github
+
+    @override
+    def __str__(self) -> str:
+        return f"{self.provider_name}:{self.username}"
+
+
+@dataclass
+class ReserveData:
+    ssh_keys: list[SshKeyProvider]
+    # how many seconds to reserve
+    timeout: int
+
+
+@dataclass
+class FirmwareUpdateData:
+    version: Literal["latest"]
+    ignore_failure: bool
+
+
+@dataclass
+class TestflingerJob:
+    job_queue: str
+    tags: list[str] | None = None
+    global_timeout: int = 14400
+    output_timeout: int = 900
+
+    provision_data: ProvisionData | None = None
+    firmware_update_data: FirmwareUpdateData | None = None
+    test_data: TestData | None = None
+    allocate_data = None
+    reserve_data: ReserveData | None = None
+
+    allocation_timeout: int = 7200
+    job_status_webhook: str | None = None
+    job_priority: int = 0
--- src/testflinger-yaml-sdk/models/provision_data.py
+++ src/testflinger-yaml-sdk/models/provision_data.py
@@ -1,16 +1,37 @@
 from dataclasses import dataclass
+from pathlib import Path
 from urllib3.util import Url
 
 
 @dataclass
-class OEMAutoinstallProvisionData:
-    """Schema for the `provision_data` section of a OEM Autoinstall job."""
+class Attachment:
+    local: Path  # verifiable, can check if the file exists, readable etc.
+    agent: Path | None  # unverifiable, can only do basic checks
 
+
+@dataclass
+class UrlProvisionData:
     url: Url
-    # attachments = fields.List(fields.Nested(Attachment), required=False)
-    token_file: str | None
-    user_data: str | None
-    redeploy_cfg: str | None
-    authorized_keys: str | None
-    zapper_iso_url: Url | None
-    zapper_iso_type: str | None
+    attachments: list[Attachment] = []
+
+
+@dataclass
+class DistroProvisionData:
+    distro: str = "noble"
+    kernel: str | None = None
+    attachments: list[Attachment] = []
+
+
+@dataclass
+class OEMAutoInstallProvisionData:
+    url: Url
+    # token_file must be specified if url requires auth
+    token_file: Path 
+    # user_data should contain directives for autoinstall and cloud-init
+    # https://canonical-testflinger.readthedocs-hosted.com/en/latest/
+    # reference/device-connector-types.html#oem-autoinstall
+    user_data: Path
+    # authorized keys file will literally be copied to the DUT
+    authorized_keys: Path
+
+type ProvisionData = DistroProvisionData | DistroProvisionData | OEMAutoInstallProvisionData
