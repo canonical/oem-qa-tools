90086b07006bb1e323bc83f03bbb590a8c94475a
Zhongning Li <zhongning.li@canonical.com>
2025-07-16 11:48:56 +0800

    fix: consistent naming, add enum

--- src/testflinger-yaml-sdk/models/test_data.py
+++ src/testflinger-yaml-sdk/models/test_data.py
@@ -1,5 +1,8 @@
 from dataclasses import dataclass
+from enum import StrEnum
 from importlib.resources import read_text
+from pathlib import Path
+from typing import Literal, final
 
 
 @dataclass
@@ -8,17 +11,48 @@ class TestData:
     test_cmds: list[str] | str
 
 
+@final
+class BuiltInTestSteps(StrEnum):
+    INITIAL = "00_initial"
+    DIST_UPGRADE = "01_dist_upgrade"
+    INSTALL_CHECKBOX_DEB = "02_install_checkbox_deb"
+    INSTALL_CHECKBOX_SNAP = "02_install_checkbox_snap"
+    INSTALL_CHECKBOX_ON_TF_AGENT = "15_install_checkbox_on_agent"
+    BEFORE_TEST = "20_before_test"
+    START_TEST = "90_start_test"
+
+
 class TestCommandBuilder:
     """
     An OOP builder for the test_cmds section
-    - It uses the shell scripts in the template/shell_scripts 
-      directory of this library to build the final test_cmds
+    - The basic idea is to separate each step in test_cmds into smaller shell
+      files to allow them to be combined in a modular way
+    - The built-in ones are loaded by their number prefix
     - The scripts are combined in alphabetical order (hence the number prefix)
     """
 
     # use importlib.resources to read this
     TEMPLATE_DIR = "template/shell_scripts/"
 
-    # def __init__(self) -> None:
-    #     t = read_text("testflinger-yaml-sdk", self.TEMPLATE_DIR)
-    #     print(t)
+    def __init__(self, checkbox_type: Literal["snap", "deb"]) -> None:
+        # have to get checkbox_type from the constructor since only 1 of them
+        # should be installed
+        assert checkbox_type in ("snap", "deb")
+        self.checkbox_type = checkbox_type
+        self.inserted_command_files: dict[int, Path] = {}
+
+    def insert_command_file(self, index: int, file_path: Path) -> None:
+        """
+        Inserts the commands in file_path at the specified index
+        - Check the 00_initial file to see built-in functions like _run, _put
+
+        :param index: insert before this index
+        :param file_path: path to the shell file
+        :rases ValueError: when index is already taken
+        """
+        # t = read_text("testflinger-yaml-sdk", self.TEMPLATE_DIR)
+        if index in self.inserted_command_files:
+            raise ValueError(f"{index} is already taken")
+        self.inserted_command_files[index] = file_path
+
+    def finish_build(self) -> TestData: ...
