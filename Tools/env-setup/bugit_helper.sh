6b0c1779e1e733386961114015dd25180e054b27
hanhsuan <hanhsuan.lee@canonical.com>
2025-09-22 10:55:43 +0800

    1. migrate from sku to canonical_id 2. migrate from bugit v1 to bugit v2

--- Tools/env-setup/bugit_helper.sh
+++ Tools/env-setup/bugit_helper.sh
@@ -6,11 +6,11 @@ C3_PROXY=""
 MACHINE_INFO=$(curl -sL "${C3_PROXY}/api/v2/machines/${CID}")
 
 echo "${MACHINE_INFO}" | jq -r \
-'. | .projects[0].name |= ascii_upcase | "#!/bin/bash\nbugit -p \(.projects[0].name) --platform-tags \"\(.launchpad_tag)\" -k \(.sku) -c \(.canonical_id) --jira"' \
+'. | .projects[0].name |= ascii_upcase | "#!/bin/bash\nbugit.bugit-v2 jira -p \(.projects[0].name) --platform-tags \"\(.launchpad_tag)\" -k \(.canonical_label) -c \(.canonical_id)"' \
 > jira_bug.sh
 
 echo "${MACHINE_INFO}" | jq -r \
-'. | .projects[0].name |= ascii_downcase | "#!/bin/bash\nbugit -p \(.projects[0].name) -t \"\(.launchpad_tag)\" -k \(.sku) -c \(.canonical_id)"' \
+'. | .projects[0].name |= ascii_downcase | "#!/bin/bash\nbugit.bugit-v2 -p \(.projects[0].name) -t \"\(.launchpad_tag)\" -k \(.canonical_label) -c \(.canonical_id)"' \
 > lp_bug.sh
 
-echo "${MACHINE_INFO}" | jq -r '. | {canonical_id, secure_id, sku}' > hardware_info.json
+echo "${MACHINE_INFO}" | jq -r '. | {canonical_id, secure_id, canonical_label}' > hardware_info.json
