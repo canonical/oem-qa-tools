6fb8ce01886713d8bdf042a2b49c61878780164a
hanhsuan <hanhsuan.lee@canonical.com>
2025-10-14 10:25:19 +0800

    first draft

---
+++ charms/wol-server/README.md
@@ -0,0 +1,24 @@
+# WoL
+
+Used to set up a Wake-up on LAN server
+
+## prerequestment
+
+Please note you should have an environment for juju machine charm to deploy,
+such as lxd
+
+### Download libraries for this charm
+
+```bash
+charmcraft fetch-lib
+```
+
+### Build this charm
+
+```bash
+charmcraft pack
+```
+
+## How to deploy
+
+juju deploy -m [juju model name] ./bundle.yaml --debug --verbose
---
+++ charms/wol-server/src/charm.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""Charm the application."""
+
+from charms.operator_libs_linux.v0 import apt
+import subprocess
+from subprocess import CalledProcessError
+import urllib.request
+import logging
+import ops
+from ops.model import (
+    BlockedStatus,
+)
+
+logger = logging.getLogger(__name__)
+default_wol_server_download_url = "https://raw.githubusercontent.com/canonical/oem-qa-tools/refs/heads/main/Tools/env-setup/wol_server.py"
+wol_install_destination = "/usr/bin/"
+
+
+class WolCharm(ops.CharmBase):
+    """Charm the application."""
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+        framework.observe(self.on.start, self._on_start)
+        framework.observe(self.on.install, self._on_install)
+
+    def _on_start(self, event: ops.StartEvent):
+        """Handle start event."""
+        self.unit.status = ops.ActiveStatus()
+
+    def _on_install(self, event: ops.InstallEvent):
+        """Handle install event."""
+        self.unit.status = ops.MaintenanceStatus("Installing needed packages")
+        self.install_apt_packages(
+            [
+                "wakeonlan",
+                "python3-fastapi",
+                "uvicorn",
+            ]
+        )
+        self.download_default_wol_server_code()
+        self.start_wol_server(self.config.get("port"))
+        self.unit.status = ops.ActiveStatus("Ready")
+
+    def _on_update(self, event: ops.UpgradeEvent):
+        """Handle install event."""
+        self.unit.status = ops.MaintenanceStatus("Updating the wol server")
+        self.download_default_wol_server_code()
+        self.start_wol_server(self.config.get("port"))
+        self.unit.status = ops.ActiveStatus("Ready")
+
+    def install_apt_packages(self, packages: list):
+        """Simple wrapper around 'apt-get install -y"""
+        try:
+            apt.update()
+            apt.add_package(packages)
+        except apt.PackageNotFoundError:
+            logger.error(
+                "a specified package not found in package cache or on system"
+            )
+            self.unit.status = BlockedStatus("Failed to install packages")
+        except apt.PackageError:
+            logger.error("could not install package")
+            self.unit.status = BlockedStatus("Failed to install packages")
+
+    def download_default_wol_server_code(self):
+        """Download default WoL server script"""
+        try:
+            urllib.request.urlretrieve(
+                default_wol_server_download_url,
+                f"{wol_install_destination}wol_server.py",
+            )
+            logger.info(f"Downloaded file to {wol_install_destination}")
+        except Exception as e:
+            logger.error(f"Failed to download file: {repr(e)}")
+            self.unit.status = BlockedStatus("Failed to install packages")
+
+    def start_wol_server(self, port: int):
+        """Start default WoL server"""
+        service_content = f"""
+        [Unit]
+        Description=Wake-on-Lan server
+        After=network.target
+
+        [Service]
+        ExecStart=uvicorn --app-dir {wol_install_destination} wol_server:app --host 0.0.0.0 --port {port}
+        Restart=on-failure
+
+        [Install]
+        WantedBy=multi-user.target
+        """
+        service_file = "/etc/systemd/system/wol.service"
+        with open(service_file, "w") as f:
+            f.write(service_content)
+
+        try:
+            subprocess.check_call(["systemctl", "daemon-reload"])
+            subprocess.check_call(["systemctl", "enable", "wol"])
+            subprocess.check_call(["systemctl", "start", "wol"])
+        except CalledProcessError as e:
+            logger.error(f"Failed to start wol server: {repr(e)}")
+            self.unit.status = BlockedStatus("Failed to setup service")
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(OemQaVmCharm)
