2956a4822f00ea73de9eb81fbcc466d541d380f6
hanhsuan <32028620+hanhsuan@users.noreply.github.com>
2026-03-04 10:28:28 +0800

    Install stress-ng from colin king PPA (#129)
    * first draft
    
    * fix flake8 issue
    
    * fix file create issue
    
    * first working commit

--- Tools/PC/oem-qa-checkbox-installer/bin/boxer.py
+++ Tools/PC/oem-qa-checkbox-installer/bin/boxer.py
@@ -7,22 +7,29 @@ import subprocess
 import time
 
 VERSION = "2.2"
-PROVIDERS = ("somerville",
-             "sutton",
-             "stella",
-             "kittyhawk",
-             "otaru",
-             "wenshan",
-             )
-CHECKBOX_REPOS = {"stable": "ppa:checkbox-dev/stable",
-                  "testing": "ppa:checkbox-dev/beta",
-                  "daily": "ppa:checkbox-dev/edge"}
+PROVIDERS = (
+    "somerville",
+    "sutton",
+    "stella",
+    "kittyhawk",
+    "otaru",
+    "wenshan",
+)
+CHECKBOX_REPOS = {
+    "stable": "ppa:checkbox-dev/stable",
+    "testing": "ppa:checkbox-dev/beta",
+    "daily": "ppa:checkbox-dev/edge",
+}
 FWTS_REPO = "ppa:firmware-testing-team/ppa-fwts-stable"
 PC_ENABLE_REPO = "ppa:oem-solutions-engineers/pc-enablement-tools"
-OEM_REPO = "https://{username}:{password}@private-ppa.launchpad.net/" \
-           "oem-services-qa/ppa/ubuntu"
-OEM_SOURCE_LIST = "deb https://private-ppa.launchpad.net/" \
-                  "oem-services-qa/ppa/ubuntu {codename} main"
+OEM_REPO = (
+    "https://{username}:{password}@private-ppa.launchpad.net/"
+    "oem-services-qa/ppa/ubuntu"
+)
+OEM_SOURCE_LIST = (
+    "deb https://private-ppa.launchpad.net/"
+    "oem-services-qa/ppa/ubuntu {codename} main"
+)
 
 # Public key for OEM Services PPA (PUBKEY 17B878BE09D5DC1F)
 OEM_PPA_GPG = """
@@ -45,27 +52,38 @@ OqboGUSfWwcOY7fN98NQj1aJGCiDr2Jy9tE=
 
 # Colors for messages output
 class TColors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 def main():
     print("== Boxer v{} ==".format(VERSION))
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="Command to run (default: install)",
-                        nargs="?", default="install")
-    parser.add_argument("-p", "--provider",
-                        help="Provider name ({})".format(", ".join(PROVIDERS)))
-    parser.add_argument("-r", "--repository",
-                        help="Checkbox repository to use ({})"
-                             .format(", ".join(CHECKBOX_REPOS)))
+    parser.add_argument(
+        "command",
+        help="Command to run (default: install)",
+        nargs="?",
+        default="install",
+    )
+    parser.add_argument(
+        "-p",
+        "--provider",
+        help="Provider name ({})".format(", ".join(PROVIDERS)),
+    )
+    parser.add_argument(
+        "-r",
+        "--repository",
+        help="Checkbox repository to use ({})".format(
+            ", ".join(CHECKBOX_REPOS)
+        ),
+    )
     args = parser.parse_args()
 
     config = configparser.ConfigParser()
@@ -81,24 +99,31 @@ def main():
     # We remove any existing PPA before adding the new ones
     setup_public_ppa(repository, username, ppa_password, remove=True)
     setup_public_ppa(repository, username, ppa_password)
+    setup_stress_ng_ppa("ppa:colin-king/stress-ng")
     setup_oem_ppa(username, ppa_password)
     install(provider)
 
 
 def create_config():
-    """ If boxer config file not found, this will ask the user a few questions
+    """If boxer config file not found, this will ask the user a few questions
     and create one next to the boxer script."""
 
-    print("Hi! It looks like there is no boxer.conf file yet."
-          " Let's create one!")
+    print(
+        "Hi! It looks like there is no boxer.conf file yet."
+        " Let's create one!"
+    )
     username = input("What's your Launchpad username? ")
     print()
-    print("Let's find the password "
-          "you need to access the OEM providers repository.")
+    print(
+        "Let's find the password "
+        "you need to access the OEM providers repository."
+    )
     # The link to personal private ppa subscription management
     # <https://launchpad.net/~oem-services-qa/+archive/ubuntu/ppa>
-    print("Go to "
-          f"<https://launchpad.net/~{username}/+archivesubscriptions/10011>")
+    print(
+        "Go to "
+        f"<https://launchpad.net/~{username}/+archivesubscriptions/10011>"
+    )
     print("You should see something like")
     print()
     print(f"\tdeb https://{username}:<password>@private-ppa...")
@@ -109,8 +134,9 @@ def create_config():
     print(", ".join(PROVIDERS))
     provider = input("What provider do you want to use by default? ")
     print()
-    print("Checkbox can be installed from the following repositories: ",
-          end="")
+    print(
+        "Checkbox can be installed from the following repositories: ", end=""
+    )
     print(", ".join(CHECKBOX_REPOS))
     repository = input("Which repository do you want to use by default? ")
 
@@ -122,14 +148,43 @@ def create_config():
     boxercfg["provider"] = provider
     boxercfg["repository"] = repository
 
-    with open('./conf/setting.conf', 'w') as configfile:
+    with open("./conf/setting.conf", "w") as configfile:
         config.write(configfile)
     print()
     print("All set!")
-    print("Next time you run boxer, "
-          "make sure your boxer.conf is located in the same directory!")
+    print(
+        "Next time you run boxer, "
+        "make sure your boxer.conf is located in the same directory!"
+    )
     print()
     time.sleep(3)
+
+
+def setup_stress_ng_ppa(ppa):
+    """
+    Setup required stress-ng PPAs to install Checkbox OEM stack.
+    By default, it adds the PPAs. If `remove` is set, the PPAs are removed.
+    """
+    print("Setting up the stress-ng PPAs...")
+    print("Removing installed stress-ng ...")
+    run_command("sudo apt remove stress-ng -y")
+    print(f"Adding PPA {ppa}...")
+    command = f"sudo add-apt-repository -y {ppa}"
+    run_command(command)
+
+    # disable the stress-ng in the checkbox-dev PPA
+    pin_content = """
+Package: stress-ng
+Pin: release o=LP-PPA-checkbox-dev-beta
+Pin-Priority: -1
+"""
+    with open("/tmp/no-stress-ng-from-checkbox-dev", "w") as f:
+        f.write(pin_content)
+
+    pin_file = "/etc/apt/preferences.d/no-stress-ng-from-checkbox-dev"
+    command = f"sudo cp /tmp/no-stress-ng-from-checkbox-dev {pin_file}"
+    run_command(command)
+    run_command("sudo apt update")
 
 
 def setup_public_ppa(repo, username, password, remove=False):
@@ -166,8 +221,10 @@ def add_oem_source_list():
     output = subprocess.run(cmd.split(), capture_output=True)
     ubuntu_codename = output.stdout.decode().strip()
     source_list = OEM_SOURCE_LIST.format(codename=ubuntu_codename)
-    cmd = f"sudo sh -c 'echo \"{source_list}\" > " \
-          f"/etc/apt/sources.list.d/oem-services-qa-ubuntu-ppa.list'"
+    cmd = (
+        f'sudo sh -c \'echo "{source_list}" > '
+        f"/etc/apt/sources.list.d/oem-services-qa-ubuntu-ppa.list'"
+    )
     run_command(cmd, shell=True)
     cmd = "sudo apt update"
     run_command(cmd)
@@ -178,11 +235,15 @@ def add_auth_conf(username, password):
     Add authentication data for the OEM Services PPA to auth.conf.d
     """
     print("Add authentication data for the OEM Services PPA to auth.conf.d...")
-    auth_conf = "machine " \
-                "private-ppa.launchpad.net/oem-services-qa/ppa/ubuntu " \
-                f"login {username} password {password}"
-    cmd = f"sudo sh -c 'echo \"{auth_conf}\" > " \
-          "/etc/apt/auth.conf.d/oem-services-qa-ubuntu-ppa.conf'"
+    auth_conf = (
+        "machine "
+        "private-ppa.launchpad.net/oem-services-qa/ppa/ubuntu "
+        f"login {username} password {password}"
+    )
+    cmd = (
+        f'sudo sh -c \'echo "{auth_conf}" > '
+        "/etc/apt/auth.conf.d/oem-services-qa-ubuntu-ppa.conf'"
+    )
     run_command(cmd, shell=True)
 
 
@@ -192,11 +253,15 @@ def add_oem_ppa_gpg():
     For more info, see:
     <https://www.linuxuprising.com/2021/01/apt-key-is-deprecated-how-to-add.html>
     """
-    print("Add OEM Services PPA public GPG key to "
-          "the trusted.gpg.d directory...")
-    cmd = f"sudo sh -c 'echo \"{OEM_PPA_GPG}\" | " \
-          "gpg --dearmor > " \
-          "/etc/apt/trusted.gpg.d/oem-services-qa-ubuntu-ppa.gpg'"
+    print(
+        "Add OEM Services PPA public GPG key to "
+        "the trusted.gpg.d directory..."
+    )
+    cmd = (
+        f'sudo sh -c \'echo "{OEM_PPA_GPG}" | '
+        "gpg --dearmor > "
+        "/etc/apt/trusted.gpg.d/oem-services-qa-ubuntu-ppa.gpg'"
+    )
     run_command(cmd, shell=True)
 
 
@@ -222,41 +287,51 @@ def pre_install():
     # Add sudoer setting file to allow Checkbox to run sudo commands without
     # having to enter the sudo password.
     user = os.getenv("USER")
-    cmd = f"echo '{user} ALL=(ALL:ALL) NOPASSWD: ALL' | " \
-          "sudo tee /etc/sudoers.d/checkbox"
+    cmd = (
+        f"echo '{user} ALL=(ALL:ALL) NOPASSWD: ALL' | "
+        "sudo tee /etc/sudoers.d/checkbox"
+    )
     run_command(cmd, shell=True)
 
     # Add GPG keys from the different repositories.
-    commands = ("sudo apt-key adv --keyserver keyserver.ubuntu.com "
-                "--recv-keys 2BBDF2BD 09D5DC1F 6BE75981",)
+    commands = (
+        "sudo apt-key adv --keyserver keyserver.ubuntu.com "
+        "--recv-keys 2BBDF2BD 09D5DC1F 6BE75981",
+    )
     print("Running pre-install commands...")
     for cmd in commands:
         run_command(cmd)
 
 
 def install(provider):
-    print("Purging Checkbox-related packages "
-          "that might already be installed...")
+    print(
+        "Purging Checkbox-related packages "
+        "that might already be installed..."
+    )
     cmd = "sudo apt-get purge -y .*plainbox.* .*checkbox.*"
     run_command(cmd)
 
     print("Installing Checkbox base packages...")
-    cmd = ("sudo DEBIAN_FRONTEND=noninteractive apt install -y "
-           "--allow-downgrades --allow-remove-essential "
-           "--allow-change-held-packages "
-           "checkbox-ng "
-           "checkbox-provider-resource "
-           "checkbox-provider-certification-client "
-           "checkbox-provider-base "
-           "canonical-certification-client")
+    cmd = (
+        "sudo DEBIAN_FRONTEND=noninteractive apt install -y "
+        "--allow-downgrades --allow-remove-essential "
+        "--allow-change-held-packages "
+        "checkbox-ng "
+        "checkbox-provider-resource "
+        "checkbox-provider-certification-client "
+        "checkbox-provider-base "
+        "canonical-certification-client"
+    )
     run_command(cmd)
 
     print("Installing provider {}...".format(provider))
     # Add DEBIAN_FRONTEND=noninteractive
     # to avoid interruption, example: postfix
-    cmd = ("sudo DEBIAN_FRONTEND=noninteractive apt install -y "
-           "--allow-downgrades --allow-remove-essential "
-           "--allow-change-held-packages plainbox-provider-oem-"+provider)
+    cmd = (
+        "sudo DEBIAN_FRONTEND=noninteractive apt install -y "
+        "--allow-downgrades --allow-remove-essential "
+        "--allow-change-held-packages plainbox-provider-oem-" + provider
+    )
     run_command(cmd)
 
 
